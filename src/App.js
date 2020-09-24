2f277b32d47cf7351ed7df6df5969ae2912314e1
Aayush Pathak <aayush.pathak16@gmail.com>
2020-09-24 15:18:46 +0530

    test #6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jenkins trigger test #5
+          Jenkins trigger test #6
         </p>
         <a
           className="App-link"
